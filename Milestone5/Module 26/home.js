b459392cea3f5193b995933c10ce4ea8d70ddf2e
Lunatic1 <112961007+TheLunatic1@users.noreply.github.com>
2025-08-23 21:29:43 +0600

    all

--- Milestone5/Module 26/home.js
+++ Milestone5/Module 26/home.js
@@ -1,12 +1,70 @@
 const validPin = 1234;
 
+//function to get input value as nember
+function getInputValue(id) {
+    const inputField = document.getElementById(id);
+    const inputValue = inputField.value;
+    const inputfieldValueNumber = parseInt(inputValue);
+
+    console.log(inputfieldValueNumber);
+    return inputfieldValueNumber;
+}
+
+//function to get input value
+function getInputValueString(id) {
+    const inputField = document.getElementById(id);
+    const inputValue = inputField.value;
+    console.log(inputValue);
+    return inputValue;
+}
+
+
+//function to get inner text
+function getInnerText(id) {
+    const element = document.getElementById(id);
+    const innerText = element.innerText;
+    console.log(innerText);
+    return innerText;
+}
+
+
+
+
+//function to set inner text
+function setInnerText(value) {
+    const element = document.getElementById("available-balance");
+    element.innerText = value;
+}
+
+
+// function to toggle
+function handleToggle(formId) {
+    const forms = document.getElementsByClassName("form12");
+    for(const form of forms) {
+        form.style.display = "none";
+    }
+    document.getElementById(formId).style.display = "block";
+}
+
+//btn style toggle
+function handleBtnToggle(btnId){
+    const formBtn = document.getElementsByClassName("form11");
+    for(const btn of formBtn) {
+        btn.classList.remove("border-[#0874f2]","bg-[#0874f20d]");
+        btn.classList.add("border-gray-300");
+    }
+    document.getElementById(btnId).classList.remove("border-gray-300");
+    document.getElementById(btnId).classList.add("border-[#0874f2]","bg-[#0874f20d]");
+
+}
+
 document.getElementById("add-money-btn").addEventListener("click", function(e) {
     e.preventDefault();
 
     const bank = document.getElementById("bank").value;
     const accountNumber = document.getElementById("account-number").value;
-    const amount = parseInt(document.getElementById("add-amount").value);
-    const pin = parseInt(document.getElementById("add-pin").value);
+    const amount = getInputValue("add-amount");
+    const pin = getInputValue("add-pin");
     const availableBalance = parseInt(document.getElementById("available-balance").innerText);
 
     console.log(amount, availableBalance);
@@ -30,7 +88,7 @@ document.getElementById("add-money-btn").addEventListener("click", function(e) {
 // cashout money feature
 document.getElementById("withdraw-btn").addEventListener("click", function(e) {
     e.preventDefault();
-    const amount = parseInt(document.getElementById("withdraw-amount").value);
+    const amount = getInputValue("withdraw-amount");
     const availableBalance = parseInt(document.getElementById("available-balance").innerText);
     const totalNewAvailableBalance = availableBalance - amount;
     console.log(totalNewAvailableBalance);
@@ -39,12 +97,23 @@ document.getElementById("withdraw-btn").addEventListener("click", function(e) {
 
 
 // toggling feature
+
 document.getElementById("add-btn").addEventListener("click", function() {
-    document.getElementById("cash-out-parent").style.display = "none"
-    document.getElementById("add-money-parent").style.display = "block"
-})
+    handleToggle("add-money-parent");
+    handleBtnToggle("add-btn");
+});
 
 document.getElementById("cashout-btn").addEventListener("click", function() {
-    document.getElementById("add-money-parent").style.display = "none"
-    document.getElementById("cash-out-parent").style.display = "block"
-})
+    handleToggle("cash-out-parent");
+    handleBtnToggle("cashout-btn");
+});
+
+document.getElementById("transfer-btn").addEventListener("click", function() {
+    handleToggle("transfer-money-parent");
+    handleBtnToggle("transfer-btn");
+});
+
+document.getElementById("transaction-btn").addEventListener("click", function() {
+    handleToggle("transaction-history-parent");
+    handleBtnToggle("transaction-btn");
+});
